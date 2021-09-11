0f73069b921fef6e91c34775563200005ea99a6c
nefanov <efanov@phystech.edu>
2021-09-11 14:41:05 +0300

    Add files via upload

---
+++ task1/examples/10_exec_pipes.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+void seq_pipe(char ***cmd)
+{
+  int   p[2];
+  pid_t pid;
+  int   fd_in = 0;
+  int   i = 0;
+
+  while (cmd[i] != NULL) {
+    pipe(p);
+    if ((pid = fork()) == -1) {
+          exit(1);
+    } else if (pid == 0) {
+        dup2(fd_in, 0);
+        if (cmd[i+1] != NULL)
+          dup2(p[1], 1);
+        close(p[0]);
+        execvp((cmd)[i][0], cmd[i]);
+        exit(2);
+    } else {
+      wait(NULL);
+      close(p[1]);
+      fd_in = p[0];
+        i++;
+    }
+}
+  return;
+}
+
+int main()
+{
+  char *ls[] = {"ls", NULL};
+  char *wc1[] = {"wc", NULL};
+  char *wc2[] = {"wc", NULL};
+  char **cmd[] = {ls, wc1, wc2, NULL};
+  seq_pipe(cmd);
+}
