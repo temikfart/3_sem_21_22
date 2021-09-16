e26f48346a2e66c3f69dcb478f32c59a4249e8d6
Artyom Fartygin <fartygin.a@phystech.edu>
2021-09-17 00:54:30 +0300

    t1: parse_tokens func.

--- task1/shell-wrapper/main.c
+++ task1/shell-wrapper/main.c
@@ -8,6 +8,46 @@
 
 #define BUFFER_SIZE 2048*1024
 
+typedef struct Command {
+  char ** argv;
+  int argc;
+}Command;
+
+typedef struct CommandLine {
+  Command * cmds;
+  int size;
+}CommandLine;
+
+CommandLine parse_tokens(char ** tokens, int num) {
+  CommandLine res;
+
+  res.size = num;
+  int cmds_sz = (int)(res.size * sizeof(Command));
+  res.cmds = malloc(cmds_sz);
+  int argv_sz[res.size];
+
+  for(int a = 0; a < res.size; a++) {
+    argv_sz[a] = 0;
+    res.cmds[a].argv = NULL;
+  }
+
+  int i, j;
+  char delim[] = " ";
+  for(i = 0; i < res.size; i++) {
+    j = 0;
+    for (char *p = strtok(tokens[i], delim); p != NULL; p = strtok(NULL, delim)) {
+      argv_sz[i] += sizeof(char *);
+      res.cmds[i].argv = realloc(res.cmds[i].argv, argv_sz[i]);
+      res.cmds[i].argv[j] = strdup(p);
+      j++;
+    }
+    res.cmds[i].argc = j;
+  }
+
+
+  return res;
+}
+
 char ** scan_cmd(int * num) {
   char ** res = NULL;
 
@@ -43,33 +83,22 @@ int main() {
   char ** buf = scan_cmd(&max_elem);
 
   // Печать массива отдельных команд
-  for(int k = 0; k < max_elem; k++) {
+  for (int k = 0; k < max_elem; k++) {
     printf("buf[%d]: %s\n", k, buf[k]);
   }
 
-//  const pid_t pid = fork();
-//
-//  // Обработка ошибок
-//  if (pid < 0) {
-//    printf("fork() error\n");
-//    return -1;
-//  }
-//
-//  if (pid > 0) {
-//    int status;
-//    waitpid(pid, &status, 0);
-//    printf("Ret code: %d\n", WEXITSTATUS(status));
-//  } else {
-////    execl("/bin/pwd", "pwd", NULL);
-//    for (char c = 'n'; tolower(c) != 'y'; scanf("%c", &c)) {
-//      printf("Kill me?(y/n):\n");
-//    }
-//    exit(42);
-//  }
+  // Парсинг команд
+  CommandLine cmds = parse_tokens(buf, max_elem);
 
-  for(int i = 0; i < max_elem; i++) {
+  // Освобождение памяти
+  for (int i = 0; i < cmds.size; i++) {
     free(buf[i]);
+    for (int j = 0; j < cmds.cmds[i].argc; j++) {
+      free(cmds.cmds[i].argv[j]);
+    }
+    free(cmds.cmds[i].argv);
   }
   free(buf);
+  free(cmds.cmds);
   return 0;
 }
