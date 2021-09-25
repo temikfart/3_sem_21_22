ea52efcdc1abbad7a0ffa92a45c7c6ec07242aff
Artyom Fartygin <fartygin.a@phystech.edu>
2021-09-25 18:24:14 +0300

    t2: Initial commit.

---
+++ task2/duplex-pipe/src/dp_main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "dp_main.h"
+
+int main() {
+  return 0;
+}
