b9ad28d531629351e8f86a4b5d6172afd02bfe95
Artyom Fartygin <fartygin.a@phystech.edu>
2021-10-01 18:33:49 +0300

    t2: OOP elements for strings.

--- task2/duplex-pipe/include/dp_main.h
+++ task2/duplex-pipe/include/dp_main.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#define BUF_SZ 1024*1024
+
+typedef struct pString String;
+typedef struct pString {
+  char* data;
+  // Methods:
+  size_t (*len)(String *self);
+  void (*delete)(String *self);
+} String;
+
+// String methods
+size_t f_length(String *self);
+void f_delete(String *self);
+
+String *ctorString(int data_sz);
+
--- task2/duplex-pipe/src/dp_main.c
+++ task2/duplex-pipe/src/dp_main.c
@@ -7,10 +7,29 @@
 #include <sys/wait.h>
 #include "dp_main.h"
 
+size_t f_length(String *self) {
+  return strlen(self->data);
+}
+void f_delete(String *self) {
+  free(self->data);
+  free(self);
+}
+String *ctorString(int data_sz) {
+  String *str = malloc(sizeof(String));
+  str->data = calloc(data_sz, sizeof(char));
+
+  // Methods
+  str->len = f_length;
+  str->delete = f_delete;
+
+  return str;
+}
+
 int main() {
-  char str1[100] = "\0";
-  char str2[100] = "\0";
+  String* str1 = ctorString(BUF_SZ);
+  String* str2 = ctorString(BUF_SZ);
   int fd1[2], fd2[2];
+
   if ( (pipe(fd1) < 0) || (pipe(fd2) < 0) ) {
     perror("Invalid pipe");
     return 1;
@@ -21,28 +40,34 @@ int main() {
       perror("Invalid fork");
       return 1;
     case 0:     /* Child process */
-      // The first fd:
+      // The first fd --- read end
       close(fd1[1]);
-      read(fd1[0], str1, sizeof(str1));
-      printf("(C) Received: %s\n", str1);
-      // The second fd:
+      read(fd1[0], str1->data, BUF_SZ);
+      printf("(C) Received: %s\n", str1->data);
+
+      // The second fd --- write end
       close(fd2[0]);
-      strcat(str2, "Hi there!");
-      write(fd2[1], str2, sizeof(str2));
-      printf("(C) Send: %s\n", str2);
+      strcat(str2->data, "Hi there!");
+      write(fd2[1], str2->data, str2->len(str2));
+      printf("(C) Send: %s\n", str2->data);
+
       break;
     default:    /* Parent process */
-      // The first fd:
+      // The first fd --- write end
       close(fd1[0]);
-      strcat(str1, "Hello");
-      write(fd1[1], str1, sizeof(str1));
-      printf("(P) Send: %s\n", str1);
-      // The second fd:
+      strcat(str1->data, "Hello");
+      write(fd1[1], str1->data, str1->len(str1));
+      printf("(P) Send: %s\n", str1->data);
+
+      // The second fd --- read end
       close(fd2[1]);
-      read(fd2[0], str2, sizeof(str2));
-      printf("(P) Received: %s\n", str2);
+      read(fd2[0], str2->data, BUF_SZ);
+      printf("(P) Received: %s\n", str2->data);
+
       break;
   }
 
+  str1->delete(str1);
+  str2->delete(str2);
   return 0;
 }
