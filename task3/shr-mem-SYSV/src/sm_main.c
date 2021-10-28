206e8f853fc987841c5219a6d9ea4b9995953c31
Artyom Fartygin <fartygin.a@phystech.edu>
2021-10-29 00:08:05 +0300

    t3(shr-mem-SYSV): complete I/O without sync.

--- task3/shr-mem-SYSV/src/sm_main.c
+++ task3/shr-mem-SYSV/src/sm_main.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <semaphore.h>
 #include "sm_main.h"
 
-
+/*
+ * 1st byte -- letter count
+ * 2nd byte -- number of packages
+ * 3rd byte -- number of bytes in the last package
+ */
 #define PCG_SZ 128
-#define SHM_SZ (8*PCG_SZ)
+#define SHM_SZ (7 * PCG_SZ + 3)
+#define BUF_SZ (SHM_SZ - 3)
 
-char *getaddr(const char *path) {
+void *getaddr(const char *path, size_t shm_sz) {
   key_t key = ftok(path, 1);
   if (key == -1) {
     perror("ftok error for");
     exit(1);
   }
 
-  int shmid = shmget(key, SHM_SZ, IPC_CREAT);
+  int shmid = shmget(key, shm_sz, IPC_CREAT);
   if (shmid == -1) {
-    perror("shmget error");
+    printf("%d, %ld", key, shm_sz);
+    perror(" shmget error");
     exit(1);
   }
 
@@ -32,99 +38,155 @@ char *getaddr(const char *path) {
     exit(1);
   }
 
+  shmptr[0] = 1;
+  shmptr[1] = 1;
+  shmptr[2] = 1;
+
+
   return shmptr;
 }
+void sempost(int *sem) {
+  int i = 0;
+  while (*sem == 0) {
+    printf("(%d) Waiting..\n", i);
+    i++;
+    sleep(1);
+  }
+  (*sem)--;
+  printf("(%d) Closing..\n", i++);
+  printf("(%d) Closed!\n", i++);
+}
+void semwait(int *sem) {
+  (*sem)++;
+  printf("Opened!\n");
+}
 void send(char *argv[]) {
-  char *shmptr = getaddr(argv[1]);
+  // Get ptr to buffer
+  char *shmptr = (char *)getaddr(argv[1], SHM_SZ);
 
-  size_t shm_sz = SHM_SZ / sizeof(char);
+  // Get ptr to two semaphores
+  // sem[0] is a semaphore for this process
+  // sem[1] is ready-flag (3 if both processes are ready)
+  size_t sem_n = 1;
+  int *sem = getaddr(argv[2], sem_n);
 
-  int fd_in = open(argv[1], O_RDONLY);
-  if (fd_in == -1) {
-    perror("open fd_in error");
+  // Get file descriptor
+  int fin = open(argv[1], O_RDONLY);
+  if (fin == -1) {
+    perror("open fd error");
     exit(1);
   }
 
-  /* SHM_SZ = 1024 = 8 * 128 + 3, пусть PCG_SZ = 128
-   * Тогда
-   *    Первый байт -- номер пачки пакетов;
-   *    Второй байт -- количество пакетов;
-   *    Третий байт -- количество байтов
-   *                   в последнем пакете. */
-  int i = 1;
-  size_t r_bytes = shm_sz - 2;
-  while(r_bytes == shm_sz - 2) {
-    // Открыть семафор + pthread_cond_signal() +++ pthread_cond_wait()
-    int sem_id = semget(ftok(argv[1], 1),
-                     1,
-                     O_CREAT | O_RDWR);
+  // Read data from fd and write in shared memory
+  size_t read_sz = BUF_SZ;
+  char *letter = shmptr;
+  *letter = (char)0;
+  char *pcg_n = shmptr + 1;
+  char *last_pcg_sz = shmptr + 2;
+  while (read_sz == BUF_SZ) {
+    // -----CRITICAL SECTION----- //
+    sempost(sem);
 
-    // Подготовка к подсчету пакетов
-    shmptr[0] = (char)(i++);
-    shmptr[1] = '0';
-    shmptr[2] = '0';
-    i %= 128;
+//    if (sem[1] != 3) {
+//      sem[1] += 1;
+//    }
 
-    // Считывание
-    r_bytes = read(fd_in, shmptr+3, shm_sz - 2);
-    printf("(C) r_bytes = %ld; shmptr = %s\n", r_bytes, shmptr+3);
+    // Read data from fd
+    read_sz = read(fin, shmptr+3, BUF_SZ);
 
-    // Считаем пакеты
-    if (r_bytes == 0) {
-      // Ничего не считано
-      shmptr[0] = '\0';
-    } else if (r_bytes < PCG_SZ) {
-      // Считано меньше 1 пакета
-      shmptr[1] = 1;
-      shmptr[2] = (char)r_bytes;
+    // Count size of data and letter number
+    if (read_sz == 0) {
+      *pcg_n = 0;
+      *last_pcg_sz = 0;
     } else {
-      // Считано больше 1 пакета
-      shmptr[1] = (char)(r_bytes / PCG_SZ + 1);
-      shmptr[2] = (char)(r_bytes % PCG_SZ + 1); // При обратном считывании: -1.
+      (*letter)++; // TODO: may be overflow
+      *pcg_n = (char)(read_sz / PCG_SZ);
+      *last_pcg_sz = (char)(read_sz % PCG_SZ);
     }
-    printf("(C) shmptr[1] = %d; [2] = %d; [3] = %d\n",
-           shmptr[0], shmptr[1], shmptr[2]);
-    // Закрыть семафор
-  }
-  shmptr[0] = '\0';
 
-  close(fd_in);
+    printf("Sent letter #%d (%d*128+%d): %s\n",
+           (*letter), (*pcg_n), (*last_pcg_sz), (shmptr+3));
+
+    semwait(sem);
+    // -----------END------------ //
+  }
+
+  close(fin);
 }
-void receive(char *shmptr, char *argv[]) {
-  int fd_out = open(argv[2], O_WRONLY);
-  if (fd_out == -1) {
-    perror("open fd_out error");
+void receive(char *argv[]) {
+  // Get ptr to buffer
+  char *shmptr = (char *)getaddr(argv[1], SHM_SZ);
+
+  // Get ptr to two semaphores
+  // sem[0] is a semaphore for this process
+  // sem[1] is ready-flag (3 if both processes are ready)
+  size_t sem_n = 3;
+  int *sem = getaddr(argv[2], sem_n);
+  sem[1] += 1;
+  while (sem[1] < 3) {
+    printf("%d\t", sem[1]);
+  }
+
+  // Get file descriptor
+  int fout = open(argv[2], O_WRONLY);
+  if (fout == -1) {
+    perror("open fd error");
     exit(1);
   }
 
-  printf("(P) ");
-  int j, i = 1;
-  int r_bytes;
-  int n_pcg;
-  while(shmptr[0] > 0) {
-    j = (int)shmptr[0];
-//      printf("(P) j = %d; ", j);
-    if (j == i) {
-      i++;
-      i %= 128;
-      sleep(1);
-      n_pcg = shmptr[1];
-      if (n_pcg == 1) {
-        r_bytes = shmptr[2];
-      } else {
-        r_bytes = (int) (n_pcg * PCG_SZ + shmptr[2] - 1);
-      }
-      printf("n_pcg = %d; last = %d\n",
-             n_pcg, shmptr[2]);
-      printf("(P) shmptr: [0] = %d; [1] = %d; [2] = %d\n",
-             shmptr[0], shmptr[1], shmptr[2]);
-      write(fd_out, shmptr+3, r_bytes);
-      printf("(P) r_bytes = %d; shmptr = %s\n", r_bytes, shmptr+3);
+  // Read data from shared memory and write in fd
+  size_t read_sz;
+  int wait_letter = 1;
+  char *rcv_letter = shmptr;
+  char *pcg_n = shmptr + 1;
+  char *last_pcg_sz = shmptr + 2;
+
+  // Say to another process, that it ready
+
+  while (1) {
+    int size = (int)(*pcg_n) + (int)(*last_pcg_sz);
+    printf("(%d) ", size);
+    if (size == 0) {
+      printf("Nothing to write. End\n");
+      break;
     }
+    // -----CRITICAL SECTION----- //
+    sempost(sem);
+    printf("Try to read from shared memory\n");
+
+    // Read data from shared memory
+    if (wait_letter == (*rcv_letter)) {
+      read_sz = (size_t)((*pcg_n) * PCG_SZ + (*last_pcg_sz));
+      printf("(RD_SZ = %ld) ", read_sz);
+
+      printf("Receive letter #%d (%d*128+%d): ",
+             (*rcv_letter), (*pcg_n), (*last_pcg_sz));
+      write(fileno(stdout), shmptr+3, read_sz);
+      printf("\n");
+
+      // Write data from shared memory in file
+      {
+        size_t written_sz = write(fout, shmptr+3, read_sz);
+        if (written_sz != read_sz) {
+          printf("Bad write in file\n");
+        }
+      }
+
+      // Clear protocol data
+      if (read_sz != BUF_SZ) {
+        (*pcg_n) = 0;
+        (*last_pcg_sz) = 0;
+      }
+
+      wait_letter++;
+    }
+
+    semwait(sem);
+    // -----------END------------ //
   }
 
-  close(fd_out);
-}
+  close(fout);
+};
 
 int main(int argc, char *argv[]) {
   if (argc != 3) {
@@ -143,9 +205,7 @@ int main(int argc, char *argv[]) {
     send(argv);
   } else {
     // Parent
-    char *shmptr = getaddr(argv[1]);
-
-    receive(shmptr, argv);
+    receive(argv);
   }
 
   return 0;
