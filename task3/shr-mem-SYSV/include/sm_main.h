4012cd2d6b096c26a2681e6b352ed8cd45556d6d
Artyom Fartygin <fartygin.a@phystech.edu>
2021-10-29 17:21:07 +0300

    t3(shr-mem-SYSV): complete task.

--- task3/shr-mem-SYSV/include/sm_main.h
+++ task3/shr-mem-SYSV/include/sm_main.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "sm_sem.h"
+
+/*
+ * 1st byte -- letter count
+ * 2nd byte -- number of packages
+ * 3rd byte -- number of bytes in the last package
+ */
+#define PROTOCOL_SZ 4
+#define PCG_SZ 128
+#define SHM_SZ (6 * PCG_SZ + PROTOCOL_SZ)
+#define BUF_SZ (SHM_SZ - PROTOCOL_SZ)
+
+void *getaddr(const char *path, size_t shm_sz);
+void send(char *argv[]);
+void receive(char *argv[]);
--- task3/shr-mem-SYSV/src/sm_main.c
+++ task3/shr-mem-SYSV/src/sm_main.c
@@ -1,22 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/sem.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <semaphore.h>
 #include "sm_main.h"
-
-/*
- * 1st byte -- letter count
- * 2nd byte -- number of packages
- * 3rd byte -- number of bytes in the last package
- */
-#define PCG_SZ 128
-#define SHM_SZ (7 * PCG_SZ + 3)
-#define BUF_SZ (SHM_SZ - 3)
 
 void *getaddr(const char *path, size_t shm_sz) {
   key_t key = ftok(path, 1);
@@ -38,37 +20,21 @@ void *getaddr(const char *path, size_t shm_sz) {
     exit(1);
   }
 
-  shmptr[0] = 1;
-  shmptr[1] = 1;
-  shmptr[2] = 1;
-
+  shmptr[0] = 0;
+  shmptr[3] = 0;
 
   return shmptr;
-}
-void sempost(int *sem) {
-  int i = 0;
-  while (*sem == 0) {
-    printf("(%d) Waiting..\n", i);
-    i++;
-    sleep(1);
-  }
-  (*sem)--;
-  printf("(%d) Closing..\n", i++);
-  printf("(%d) Closed!\n", i++);
-}
-void semwait(int *sem) {
-  (*sem)++;
-  printf("Opened!\n");
 }
 void send(char *argv[]) {
   // Get ptr to buffer
   char *shmptr = (char *)getaddr(argv[1], SHM_SZ);
 
-  // Get ptr to two semaphores
-  // sem[0] is a semaphore for this process
-  // sem[1] is ready-flag (3 if both processes are ready)
-  size_t sem_n = 1;
-  int *sem = getaddr(argv[2], sem_n);
+  int *semid = MySemOpen(argv[1]);
+  if (semid == NULL) {
+    perror("MySemOpen error");
+    exit(1);
+  }
+//  printf("(C): semid = %d\n", *semid);
 
   // Get file descriptor
   int fin = open(argv[1], O_RDONLY);
@@ -80,52 +46,54 @@ void send(char *argv[]) {
   // Read data from fd and write in shared memory
   size_t read_sz = BUF_SZ;
   char *letter = shmptr;
-  *letter = (char)0;
   char *pcg_n = shmptr + 1;
   char *last_pcg_sz = shmptr + 2;
+  char *snt_letter = shmptr + 3;
   while (read_sz == BUF_SZ) {
     // -----CRITICAL SECTION----- //
-    sempost(sem);
+    MySemWait(semid);
 
-//    if (sem[1] != 3) {
-//      sem[1] += 1;
-//    }
+    if (*snt_letter == *letter) {
+      // Read data from fd
+      read_sz = read(fin, shmptr+PROTOCOL_SZ, BUF_SZ);
 
-    // Read data from fd
-    read_sz = read(fin, shmptr+3, BUF_SZ);
-
-    // Count size of data and letter number
-    if (read_sz == 0) {
-      *pcg_n = 0;
-      *last_pcg_sz = 0;
-    } else {
+      // Count size of data and letter number
       (*letter)++; // TODO: may be overflow
-      *pcg_n = (char)(read_sz / PCG_SZ);
-      *last_pcg_sz = (char)(read_sz % PCG_SZ);
+      (*letter) = (char)((int)(*letter) % 128);
+      if (read_sz == 0) {
+        *pcg_n = 0;
+        *last_pcg_sz = 0;
+      } else {
+        *pcg_n = (char)(read_sz / PCG_SZ);
+        *last_pcg_sz = (char)(read_sz % PCG_SZ);
+      }
+
+//      printf("Sent letter #%d (%d*128+%d): ",
+//             (*letter), (*pcg_n), (*last_pcg_sz));
+//      write(fileno(stdout), shmptr+PROTOCOL_SZ, read_sz);
+//      printf("\n");
+        printf("Sent %ld\n", read_sz);
     }
 
-    printf("Sent letter #%d (%d*128+%d): %s\n",
-           (*letter), (*pcg_n), (*last_pcg_sz), (shmptr+3));
-
-    semwait(sem);
+    MySemPost(semid);
     // -----------END------------ //
   }
 
   close(fin);
+  MySemClose(semid);
+  MySemRemove(argv[1]);
+  shmdt(shmptr);
 }
 void receive(char *argv[]) {
   // Get ptr to buffer
   char *shmptr = (char *)getaddr(argv[1], SHM_SZ);
 
-  // Get ptr to two semaphores
-  // sem[0] is a semaphore for this process
-  // sem[1] is ready-flag (3 if both processes are ready)
-  size_t sem_n = 3;
-  int *sem = getaddr(argv[2], sem_n);
-  sem[1] += 1;
-  while (sem[1] < 3) {
-    printf("%d\t", sem[1]);
+  int *semid = MySemOpen(argv[1]);
+  if (semid == NULL) {
+    perror("MySemOpen error");
+    exit(1);
   }
+//  printf("(P): semid = %d\n", *semid);
 
   // Get file descriptor
   int fout = open(argv[2], O_WRONLY);
@@ -140,33 +108,35 @@ void receive(char *argv[]) {
   char *rcv_letter = shmptr;
   char *pcg_n = shmptr + 1;
   char *last_pcg_sz = shmptr + 2;
+  char *rd_letter = shmptr + 3;
 
   // Say to another process, that it ready
 
   while (1) {
-    int size = (int)(*pcg_n) + (int)(*last_pcg_sz);
-    printf("(%d) ", size);
-    if (size == 0) {
+    // -----CRITICAL SECTION----- //
+    MySemWait(semid);
+
+    int size = (int)(*pcg_n) * PCG_SZ + (int)(*last_pcg_sz);
+//    printf("(%d) ", size);
+    if (size == 0 && *rcv_letter != 0) {
       printf("Nothing to write. End\n");
       break;
     }
-    // -----CRITICAL SECTION----- //
-    sempost(sem);
-    printf("Try to read from shared memory\n");
+//    printf("Try to read from shared memory\n");
 
     // Read data from shared memory
     if (wait_letter == (*rcv_letter)) {
       read_sz = (size_t)((*pcg_n) * PCG_SZ + (*last_pcg_sz));
-      printf("(RD_SZ = %ld) ", read_sz);
+//      printf("(RD_SZ = %ld) ", read_sz);
 
-      printf("Receive letter #%d (%d*128+%d): ",
-             (*rcv_letter), (*pcg_n), (*last_pcg_sz));
-      write(fileno(stdout), shmptr+3, read_sz);
-      printf("\n");
+//      printf("Receive letter #%d (%d*128+%d): ",
+//             (*rcv_letter), (*pcg_n), (*last_pcg_sz));
+//      write(fileno(stdout), shmptr+PROTOCOL_SZ, read_sz);
+//      printf("\n");
 
       // Write data from shared memory in file
       {
-        size_t written_sz = write(fout, shmptr+3, read_sz);
+        size_t written_sz = write(fout, shmptr+PROTOCOL_SZ, read_sz);
         if (written_sz != read_sz) {
           printf("Bad write in file\n");
         }
@@ -174,18 +144,26 @@ void receive(char *argv[]) {
 
       // Clear protocol data
       if (read_sz != BUF_SZ) {
-        (*pcg_n) = 0;
-        (*last_pcg_sz) = 0;
+        *rcv_letter = 1;
+        *pcg_n = 0;
+        *last_pcg_sz = 0;
       }
 
       wait_letter++;
+      wait_letter %= 128;
+      (*rd_letter)++;
+      (*rd_letter) = (char)((int)(*rd_letter) % 128);
+      printf("Received %ld\n", read_sz);
     }
 
-    semwait(sem);
+    MySemPost(semid);
     // -----------END------------ //
   }
 
   close(fout);
+  MySemClose(semid);
+  MySemRemove(argv[1]);
+  shmdt(shmptr);
 };
 
 int main(int argc, char *argv[]) {
