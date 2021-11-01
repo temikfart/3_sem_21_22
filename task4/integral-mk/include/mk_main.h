2615f711de16a8c048ff8c3db0be91dd58a44874
Artyom Fartygin <fartygin.a@phystech.edu>
2021-11-01 16:01:17 +0300

    t4: implement generating N points via one thread.

--- task4/integral-mk/include/mk_main.h
+++ task4/integral-mk/include/mk_main.h
@@ -1,3 +1,21 @@
 #pragma once
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+#define POINTS_NUM 10
+
+typedef struct Point {
+  double x;
+  double y;
+} Point;
+
+typedef struct Interval {
+  double a;
+  double b;
+} Interval;
+
+Interval ScanInterval();
+void GeneratePoints(Point *Points, Interval Interval);
+//void SendViaShMem();
--- task4/integral-mk/src/mk_main.c
+++ task4/integral-mk/src/mk_main.c
@@ -1,5 +1,40 @@
 #include "mk_main.h"
 
+Interval ScanInterval() {
+  Interval s;
+  s.a = 0;
+  s.b = 1;
+
+  return s;
+}
+void GeneratePoints(Point *Points, Interval Interval) {
+  // Set seed to random generator
+  u_int now = time(NULL);
+  srand(now);
+
+  // Generate points into the array Points
+  double range = Interval.b - Interval.a;
+  for (int i = 0; i < POINTS_NUM; i++) {
+    // Generating points into the [a,b]^2
+    Points[i].x = (double)rand() / (RAND_MAX)
+            * range + Interval.a;               // x in [a,b]
+    Points[i].y = (double)rand() / (RAND_MAX)
+            * Interval.b;                       // y in [0,b]
+  }
+}
+
 int main() {
+  Interval Interval = ScanInterval();
+
+  // Generate POINTS_NUM points in array
+  Point *Points = (Point *)malloc(POINTS_NUM * sizeof(Point));
+  GeneratePoints(Points, Interval);
+
+  for (int i = 0; i < POINTS_NUM; i++) {
+    printf("(%2d): (%.2f;%.2f)\n", (i+1), Points[i].x, Points[i].y);
+  }
+
+  free(Points);
+
   return 0;
 }
