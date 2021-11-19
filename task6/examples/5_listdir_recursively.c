bd98edada0baa113c3dbfc694608c933ad204277
nefanov <efanov@phystech.edu>
2021-11-19 19:30:33 +0300

    Update 5_listdir_recursively.c

--- task6/examples/5_listdir_recursively.c
+++ task6/examples/5_listdir_recursively.c
@@ -1,33 +1,44 @@
-#include <unistd.h>
+/* Программа рекурсивно распечатывает полные пути к директориям и регулярным файлам, начиная с текущей директории. Данный пример немного отличается от рассмотренного на семинаре, однако смысл один и тот же */
+
+#include <stdio.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <dirent.h>
-#include <stdio.h>
 #include <string.h>
+#include <limits.h>
+#include <unistd.h>
 
-void listdir(const char *name, int indent)
-{
-    DIR *dir;
-    struct dirent *entry;
-
-    if (!(dir = opendir(name)))
-        return;
-
-    while ((entry = readdir(dir)) != NULL) {
-        if (entry->d_type == DT_DIR) {
-            char path[1024];
-            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-                continue;
-            snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
-            printf("%*s[%s]\n", indent, "", entry->d_name);
-            listdir(path, indent + 2);
-        } else {
-            printf("%*s- %s\n", indent, "", entry->d_name);
+void SearchDirectory(const char *name) {
+    DIR *dir = opendir(name);               //Assuming absolute pathname here.
+    if(dir) {
+        char Path[PATH_MAX], *EndPtr = Path;
+        struct stat info;                //Helps us know about stuff
+        struct dirent *e;
+        strcpy(Path, name);                  //Copies the current path to the 'Path' variable.
+        EndPtr += strlen(name);              //Moves the EndPtr to the ending position.
+        while ((e = readdir(dir)) != NULL) {  //Iterates through the entire directory.
+            strcpy(EndPtr, e -> d_name);       //Copies the current filename to the end of the path, overwriting it with each loop.
+            if (!stat(Path, &info)) {         //stat returns zero on success.
+                if (S_ISDIR(info.st_mode)) {  //Are we dealing with a directory?
+                    //Make corresponding directory in the target folder here.
+                    printf("directory: %s/\n", Path);
+                    ;
+                    SearchDirectory(Path);   //Calls this function AGAIN, this time with the sub-name.
+                } else if (S_ISREG(info.st_mode)) { //Or did we find a regular file?
+                    ;
+                    //Run Copy routine
+                    printf("reg_file: %s\n", Path);
+                }
+            }
         }
     }
-    closedir(dir);
+    return;
 }
 
-int main(void) {
-    listdir(".", 0);
+int main() {
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        SearchDirectory(strcat(cwd, "/"));
+    }
     return 0;
 }
