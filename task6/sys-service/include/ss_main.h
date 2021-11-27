5bade2540caa4e508168da2eeca6ddf45f65a505
Artyom Fartygin <fartygin.a@phystech.edu>
2021-11-27 21:42:55 +0300

    t6: parsing console's arguments.

--- task6/sys-service/include/ss_main.h
+++ task6/sys-service/include/ss_main.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <getopt.h>
+
+void print_instruction(const char* cmd);
+void parse_console_args(int argc, const char* argv[]);
--- task6/sys-service/src/ss_main.c
+++ task6/sys-service/src/ss_main.c
@@ -1,5 +1,43 @@
 #include "ss_main.h"
 
-int main() {
+void print_instruction(const char* cmd) {
+  printf("Usage: %s [PID] [OPTIONS]\n", cmd);
+  printf("PID:\n");
+  printf("%20s -- pid of tracking process\n", "pid");
+  printf("OPTIONS:\n");
+  printf("%20s -- start the system service as a daemon\n", "-d, --daemon");
+}
+
+void parse_console_args(int argc, const char* argv[]) {
+  int flag = 0;
+  opterr = 0;
+
+  // If without arguments
+  if (argc == 1) {
+    print_instruction(argv[0]);
+    exit(0);
+  }
+
+  const char optstring[] = "d";
+  const struct option longopts[] =
+          {{"daemon", 0, NULL, 'd'}};
+  while ((flag = getopt_long(argc, argv, optstring, longopts, NULL)) != -1) {
+    switch (flag) {
+      case 'd':
+        printf("flag: %s\n", "-d");
+        break;
+      default:
+        printf("In parse_console_args(): "
+               "default condition was activated.\n");
+        exit(1);
+    }
+  }
+
+  // TODO: need to accept file with config.
+}
+
+int main(int argc, const char* argv[]) {
+  parse_console_args(argc, argv);
+
   return 0;
 }
